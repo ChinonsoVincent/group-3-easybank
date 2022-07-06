d48ae22a2ad0ccae194bb75f1173cde956c16703
Chinonso Vincent <nchinonsovincent@gmail.com>
2022-07-06 18:31:32 +0100

    added a remove feature to the mobile nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const links = document.querySelector(".nav-list");
 const navBtnDOM = document.querySelector(".nav-btn");
+
 navBtnDOM.addEventListener("click", () => {
-    links.classList.toggle("show-links");
+	links.classList.toggle("show-links");
 });
 
-
-
+links.addEventListener("click", () => {
+	links.classList.remove("show-links");
+});
